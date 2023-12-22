51b537613a79efef831910849d2fdaced0b4cde9
harupong <harupong@gmail.com>
2023-12-22 18:14:38 +0900

    Refactor

--- ansible/playbook/utils/healthchecks.sh
+++ ansible/playbook/utils/healthchecks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#
 # healthcheck.io
+
+set -aux
 
 API_KEY=${HC_RW_API_KEY}
 HOSTNAME=$(hostname)
@@ -7,25 +10,102 @@ HOSTNAME=$(hostname)
 ####################################
 # CHECK FOR TAILSCALE STATUS
 ## create a check for the server if not available 
-ts_url=$(curl -s --header "X-Api-Key: ${API_KEY}" "https://healthchecks.io/api/v3/checks/?slug=ts-${HOSTNAME}" | jq -r '.checks[]?.ping_url')
-if [ -z "${ts_url}" ]; then
-    TS_PAYLOAD='{"name": "ts-'$HOSTNAME'", "slug": "ts-'$HOSTNAME'", "grace": 900, "schedule": "*/10 * * * *", "tz": "Asia/Tokyo" , "unique": ["name"], "tags": "f1vpn tailscale", "channels": "*"}'
-    ts_url=$(curl --silent https://healthchecks.io/api/v3/checks/  -H "X-Api-Key: $API_KEY" -d "$TS_PAYLOAD" | jq -r '.ping_url')
-fi
+function tailscale_status() {
+    local ts_prefix="ts-"
+    local ts_url
+    ts_url=$(get_check_url ${ts_prefix})
 
-## if tailscale fails, there is no ping made
-SERVER_STATS=$(top -n 1 -b -d 2 | head -n5)
-tailscale status && curl -m 10 --retry 5 --data-raw "${SERVER_STATS}" ${ts_url}
+    if [[ -z "${ts_url}" ]]; then
+        local ts_payload
+        ts_payload=$(jq -n '{
+            "name": "'$ts_prefix''$HOSTNAME'",
+            "slug": "'$ts_prefix''$HOSTNAME'",
+            "grace": 900,
+            "schedule": "*/10 * * * *",
+            "tz": "Asia/Tokyo",
+            "unique": ["name"],
+            "tags": "f1vpn tailscale",
+            "channels": "*"}')
+        ts_url=$( \
+            curl \
+                --silent \
+                --header "X-Api-Key: $API_KEY" \
+                --data "$ts_payload" \
+                https://healthchecks.io/api/v3/checks/ \
+            | jq -r '.ping_url')
+    fi
+
+    ## if tailscale fails, there is no ping made
+    local server_stats
+    server_stats=$(top -n 1 -b -d 2 | head -n5)
+    tailscale status && curl_command "${server_stats}" "${ts_url}"
+}
 
 ####################################
 # CHECK FOR SERVER BANDWIDTH USAGE
-bw_url=$(curl -s --header "X-Api-Key: ${API_KEY}" "https://healthchecks.io/api/v3/checks/?slug=bw-${HOSTNAME}" | jq -r '.checks[]?.ping_url')
-if [ -z "${bw_url}" ]; then
-    BW_PAYLOAD='{"name": "bw-'$HOSTNAME'", "slug": "bw-'$HOSTNAME'", "grace": 43200, "schedule": "*/10 * * * *", "tz": "Asia/Tokyo" , "unique": ["name"], "tags": "f1vpn bandwidth", "channels": "*"}'
-    bw_url=$(curl --silent https://healthchecks.io/api/v3/checks/  -H "X-Api-Key: $API_KEY" -d "$BW_PAYLOAD" | jq -r '.ping_url')
-fi
+function bandwidth_status() {
+    local bw_prefix="bw-"
+    local bw_url
+    bw_url=$(get_check_url ${bw_prefix})
 
-## Check fails if monthly tx/rx total is greater than 900GB
-MONTHLY_BW_USAGE=$(sudo docker exec vnstat vnstat --oneline b | expr $(cut -d ";" -f 11) / 1024 / 1024 / 1024)
-if [ ${MONTHLY_BW_USAGE} -gt 900 ]; then bw_url=${bw_url}/fail; fi
-curl -m 10 --retry 5 --data-raw "Bandwidth used: ${MONTHLY_BW_USAGE} GB" ${bw_url}
+    if [[ -z "${bw_url}" ]]; then
+        local bw_payload
+        bw_payload=$(jq -n '{
+            "name": "'$bw_prefix''$HOSTNAME'",
+            "slug": "'$bw_prefix''$HOSTNAME'",
+            "grace": 43200,
+            "schedule": "*/10 * * * *",
+            "tz": "Asia/Tokyo",
+            "unique": ["name"],
+            "tags": "f1vpn bandwidth",
+            "channels": "*"}')
+        bw_url=$( \
+            curl \
+                --silent \
+                --header "X-Api-Key: $API_KEY" \
+                --data "$bw_payload" \
+                https://healthchecks.io/api/v3/checks/ \
+            | jq -r '.ping_url')
+    fi
+
+    ## Check fails if monthly tx/rx total is greater than 900GB
+    local monthly_bw_usage
+    monthly_bw_usage=$( \
+        sudo docker exec vnstat vnstat --oneline b \
+        | cut -d';' -f11 \
+        | awk '{ printf "%d", $1 / (1024 * 1024 * 1024) }')
+    if [[ ${monthly_bw_usage} -gt 900 ]]; then bw_url=${bw_url}/fail; fi
+    
+    status="Bandwidth used: ${monthly_bw_usage} GB"
+    curl_command "${status}" "${bw_url}"
+}
+
+#######################################
+# Get existing check url from healthchecks.io
+# Arguments:
+#   PREFIX for a slug
+#######################################
+function get_check_url() {
+    curl \
+        --silent \
+        --header "X-Api-Key: ${API_KEY}" \
+        "https://healthchecks.io/api/v3/checks/?slug=${1}${HOSTNAME}" \
+    | jq -r '.checks[]?.ping_url'
+}
+
+#######################################
+# Post status to healthchecks.io
+# Arguments:
+#   STATUS to be posted to healthchecks.io
+#   URL to post to
+#######################################
+function curl_command() {
+    curl --max-time 10 --retry 5 --data-raw "${1}" "${2}" 
+}
+
+function main() {
+    tailscale_status
+    bandwidth_status
+}
+
+main "$@"
